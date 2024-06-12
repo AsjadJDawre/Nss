d61654016b3f95e9f242a51020eae1c9ce1652fd
Dawre Asjad <155601345+AsjadJDawre@users.noreply.github.com>
2024-06-12 08:58:23 +0530

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,7 +1,7 @@
 <?php
 
 // Email configuration
-$to = 'dawreasjad72@example.com';
+$to = 'dummy1243@example.com';
 $subject = 'Test Email with Buttons';
 $message = file_get_contents('reponse_email.php');
 
@@ -10,7 +10,7 @@ $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 // Additional headers
-$headers .= 'From: projecttesting48@gmail.com.com' . "\r\n"; // Replace with your email address
+$headers .= 'From: projecttesting_email@gmail.com.com' . "\r\n"; // Replace with your email address
 
 // Send the email
 mail($to, $subject, $message, $headers);
